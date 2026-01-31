a23383d7c914e74af4af4892918ce6115232504c
Om Wani <omwani0701@gmail.com>
2026-01-31 12:49:16 -0500

    Wwise Integration

---
+++ Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID TESTEVENT = 1097980931U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMEPLAYSTATES
+        {
+            static const AkUniqueID GROUP = 574587799U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STORY1 = 949836589U;
+                static const AkUniqueID STORY2 = 949836590U;
+                static const AkUniqueID STORY3 = 949836591U;
+            } // namespace STATE
+        } // namespace GAMEPLAYSTATES
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASKBANK = 457553899U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
