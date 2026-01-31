e833d7976d68e8508ab4ad17569e860b81dab0d7
Om Wani <omwani0701@gmail.com>
2026-01-31 17:05:53 -0500

    Updated Banks

--- Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,21 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID TESTEVENT = 1097980931U;
+        static const AkUniqueID PLAY_DX_BOSSTALKING = 4255932248U;
+        static const AkUniqueID PLAY_DX_BOSSTALKING_AWFUL = 1822738380U;
+        static const AkUniqueID PLAY_DX_BOSSTALKING_GREAT = 3315181258U;
+        static const AkUniqueID PLAY_HIPPO01 = 567901711U;
+        static const AkUniqueID PLAY_SFX_CDEJECT = 125881202U;
+        static const AkUniqueID PLAY_SFX_EMAILSOUND = 3552864623U;
+        static const AkUniqueID PLAY_SFX_MOUSECLICK = 275305017U;
+        static const AkUniqueID PLAY_SFX_REDACTION = 387600705U;
+        static const AkUniqueID PLAY_SFX_VOICEMAIL = 787775641U;
+        static const AkUniqueID SET_STATE_GAMEPLAY = 2289732960U;
+        static const AkUniqueID SET_STATE_STORY2 = 3165068675U;
+        static const AkUniqueID SET_STATE_STORY3 = 3165068674U;
+        static const AkUniqueID STARTGAME = 1521187885U;
+        static const AkUniqueID STOP_SFX_REDACTION = 1988521271U;
+        static const AkUniqueID STOPGAME = 1747031839U;
     } // namespace EVENTS
 
     namespace STATES
@@ -31,7 +45,24 @@ namespace AK
             } // namespace STATE
         } // namespace GAMEPLAYSTATES
 
+        namespace GAMESTATES
+        {
+            static const AkUniqueID GROUP = 777429653U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LEVEL = 2782712965U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAMESTATES
+
     } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DIALOGUEDUCKING = 969816968U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -41,7 +72,11 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID DIALOGUE = 3930136735U;
         static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
