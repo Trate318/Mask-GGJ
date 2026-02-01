b22fc84bcf20e7edab58a07d85de0f22cee27f9c
Om Wani <omwani0701@gmail.com>
2026-02-01 04:50:11 -0500

    Wwise Banks update

--- Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ANCHOR_1B = 1421248425U;
         static const AkUniqueID PLAY_DX_BOSSTALKING = 4255932248U;
         static const AkUniqueID PLAY_DX_BOSSTALKING_AWFUL = 1822738380U;
         static const AkUniqueID PLAY_DX_BOSSTALKING_GREAT = 3315181258U;
@@ -32,19 +33,6 @@ namespace AK
 
     namespace STATES
     {
-        namespace GAMEPLAYSTATES
-        {
-            static const AkUniqueID GROUP = 574587799U;
-
-            namespace STATE
-            {
-                static const AkUniqueID NONE = 748895195U;
-                static const AkUniqueID STORY1 = 949836589U;
-                static const AkUniqueID STORY2 = 949836590U;
-                static const AkUniqueID STORY3 = 949836591U;
-            } // namespace STATE
-        } // namespace GAMEPLAYSTATES
-
         namespace GAMESTATES
         {
             static const AkUniqueID GROUP = 777429653U;
@@ -57,11 +45,25 @@ namespace AK
             } // namespace STATE
         } // namespace GAMESTATES
 
+        namespace LEVELSTATES
+        {
+            static const AkUniqueID GROUP = 837128579U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STORY1 = 949836589U;
+                static const AkUniqueID STORY2 = 949836590U;
+                static const AkUniqueID STORY3 = 949836591U;
+            } // namespace STATE
+        } // namespace LEVELSTATES
+
     } // namespace STATES
 
     namespace GAME_PARAMETERS
     {
-        static const AkUniqueID DIALOGUEDUCKING = 969816968U;
+        static const AkUniqueID RTPC_DIALOGUEDUCKING = 1145265470U;
+        static const AkUniqueID RTPC_MASTERVOLUME = 2582501215U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
